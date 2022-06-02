a0d09bf3a7dba25ef9f18b35baf4d9c91ee1f8e1
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2022-06-02 13:14:09 +0300

    updated and fixed some issues

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from os import listdir, system
 langs = []
 for l in listdir('languages'):
     if l.endswith('ts'):
-        system('lrelease-qt5 languages/%s' % l)
+        system('lrelease languages/%s' % l)
         langs.append(('languages/%s' % l).replace('.ts', '.qm'))
 
 #system("pyrcc5 resources/data.qrc -o historymanager/data_rc.py")
